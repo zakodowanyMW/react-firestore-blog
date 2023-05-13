7323d2455c584b574fb4b3b38dce0ee8990e9da9
Marcin Winiarski <winiardesign@gmail.com>
2023-05-13 18:10:36 +0200

    get data from firestore

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import {collection, getDocs} from 'firebase/firestore';
+import { db } from '../firebase-config';
 
 function Home() {
+
+  const [postList, setPostList] = useState([]);
+  const postsCollectionRed = collection(db, "posts");
+
+  useEffect(() => {
+    const getPosts = async () => {
+    const data = await getDocs(postsCollectionRed); 
+    setPostList(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
+    }
+    getPosts();
+  });
+
   return (
-    <div>Home</div>
+    <div className='homePage'>
+      {postList.map(post => {
+        return <div className='post'>{post.title}</div>
+      })}
+    </div>
   )
 }
 
